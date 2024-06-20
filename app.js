ac64dd7424053b5c9166f3499fa16097418ae591
unknown <4kaskad@gmail.com>
2024-06-20 18:12:07 +0300

    correcting code, so it will work correctly

--- app.js
+++ app.js
@@ -1,22 +1,23 @@
 const numbers = [];
-const totalNumbers = 5;
+const totalNumbers = 11;
 let guesses = 0;
-// let randomNr = Math.floor(Math.random() * totalNumbers + 1);
-let randomNr = 8;
+// let randomNr = Math.round(Math.random() * totalNumbers + 1);
+let randomNr = 10;
 let current_position;
 let direction;
 let minNumber = 0;
 let maxNumber = totalNumbers;
-console.log("randomNr", randomNr);
+
+console.log("Random number is: ", randomNr);
 
 // Filling array
 for (let i = 1; i <= totalNumbers; i++) {
   numbers.push(i);
 }
 
-// Making first guess
-function oneGuess(minMax) {
-  current_position = Math.floor(minMax / 2);
+// Making a guess
+function oneGuess() {
+  current_position = Math.floor((maxNumber + minNumber) / 2);
   guesses++;
   return current_position;
 }
@@ -25,44 +26,20 @@ function oneGuess(minMax) {
 function isRight(current_position) {
   if(current_position === randomNr){
     console.log(`Guessed in ${guesses} steps`);
-    return direction= "match"
+    return direction = "match"
   } else if(current_position > randomNr) {
     maxNumber = current_position;
-    return direction = "lower"
+    return direction = "number is lower"
   } else if(current_position < randomNr) {
     minNumber = current_position;
-    return direction = "higher"
+    return direction = "number is higher"
   }
 }
 
-// Making further guesses
-function allGuesses(direction) {
-    switch (direction) {
-      case 'lower':
-        oneGuess(maxNumber);
-        console.log("current_position", current_position);
-        break;
-      case 'higher':
-        oneGuess(minNumber);
-        console.log("current_position", current_position);
-        break;
-      case 'match':
-        console.log(`Guessed in ${guesses} steps`);
-        return "guessed"
-        break;
-    }
-  }
-
-debugger
-current_position = oneGuess(maxNumber);
-let current_direction = isRight(current_position);
-while(allGuesses(direction) !== "guessed"){
-    current_direction = isRight(current_position);
-    allGuesses(current_direction);
-};
-
-// console.log("current_direction", current_direction);
-// console.log("current_position", current_position);
-// console.log("arr", numbers);
-console.log("guesses", guesses);
-console.log("randomNr", randomNr);
+// Guessing while it will be guessed
+while(isRight(current_position) !== 'match'){
+  current_position = oneGuess();
+  console.log("current_position", current_position);
+  let current_direction = isRight(current_position);
+  console.log("current_direction", current_direction);
+}
